79aa3fadc82cec945d44488afa2076e5f6a9e96c
commit <>
2018-01-18 03:40:44 +0900

    add MainAutoLayout

---
+++ MemoBomb/MemoBomb/BlackTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BlackTableViewCell.swift
+//  MemoBomb
+//
+//  Created by lee on 2018. 1. 18..
+//  Copyright © 2018년 smith. All rights reserved.
+//
+
+import UIKit
+
+class BlackTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MemoBomb/MemoBomb/ViewController.swift
+++ MemoBomb/MemoBomb/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var memoTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "blackCell", for: indexPath) as! BlackTableViewCell
+        cell.timeLabel.text = "23:20:12"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+}
