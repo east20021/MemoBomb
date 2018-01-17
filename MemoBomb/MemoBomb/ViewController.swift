d33ee1abc2e0ada6564988dcf06e0d5a0ed3d775
commit <>
2018-01-18 03:56:39 +0900

    add AddButton

--- MemoBomb/MemoBomb/ViewController.swift
+++ MemoBomb/MemoBomb/ViewController.swift
@@ -32,7 +32,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        return 120
     }
     
 }
